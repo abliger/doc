dd382367eb8dc302831e9bd3607df004c9d44a94
abliger <3330181534@qq.com>
2022-07-16 18:35:46 +0800

    add data structure ts c code

--- data structures/introduction/src/ts/index.ts
+++ data structures/introduction/src/ts/index.ts
@@ -1,5 +1,81 @@
+import {assertEquals} from 'https://deno.land/std@0.109.0/testing/asserts.ts';
+
 let nums={
   20:[-2,11,-4,13,-5,-2],
   11:[4,-3,5,-2,-1,2,6,-2]
 }
-console.log(nums)
+let maxsubSequenceSum1 = (arr:Array<number>) => {
+  let maxNum = 0
+  for(let i = 0;i<arr.length;i++){
+    for(let j = i;j<arr.length;j++){
+      let currentNum =0;
+      for(let k=i;k<=j;k++){
+        currentNum += arr[k]
+      }
+      if(maxNum < currentNum) {
+        maxNum = currentNum
+      }
+    }
+  }
+  return maxNum
+}
+let maxsubSequenceSum2 = (arr:Array<number>) => {
+  let maxNum = 0
+  for(let i = 0;i<arr.length;i++){
+    let currentNum = 0;
+    for(let j = i;j<arr.length;j++){
+      currentNum += arr[j]
+      if(maxNum < currentNum) {
+        maxNum = currentNum
+      }
+    }
+  }
+  return maxNum  
+}
+let maxsubSequenceSum3 = (arr:Array<number>,start,end)=>{
+  if(start == end){
+    if(arr[start]>0){
+      return arr[start]
+    }
+    return 0
+  }
+  let center = Number.parseInt((start + end)/2 +'')
+  let rmax = maxsubSequenceSum3(arr,start,center)
+  let lmax = maxsubSequenceSum3(arr,center +1,end)
+  let rcmax = 0
+  let rmmax = 0
+  for(let i = center;i>=0;i--){
+    rcmax += arr[i]
+    if(rcmax > rmmax){
+      rmmax = rcmax
+    }
+  }
+  let lcmax = 0
+  let lmmax = 0
+  for(let i = center + 1;i<=end;i++){
+    lcmax += arr[i]
+    if(lcmax > lmmax){
+      lmmax = lcmax
+    }
+  }
+  return Math.max(rmax,lmax,rmmax + lmmax)
+}
+let maxsubSequenceSum4 = (arr:Array<number>) =>{
+  let currentNum = 0
+  let maxNum = 0;
+  for(let i=0;i<arr.length;i++){
+    currentNum += arr[i]
+    if(currentNum > maxNum){
+      maxNum = currentNum
+    } else if(currentNum < 0){
+      currentNum = 0
+    }
+  }
+  return maxNum
+}
+for(let key of Object.keys(nums)){
+  assertEquals(Number.parseInt(key),maxsubSequenceSum1(nums[key]))
+  assertEquals(Number.parseInt(key),maxsubSequenceSum2(nums[key]))
+  assertEquals(Number.parseInt(key),maxsubSequenceSum3(nums[key],0,nums[key].length - 1))
+  assertEquals(Number.parseInt(key),maxsubSequenceSum4(nums[key]))
+}         
